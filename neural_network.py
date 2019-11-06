ffec8a506903c85ccaad5fb0c075c0ce2635ac35
Guillermo Del Valle <gdelvalle@nevada.unr.edu>
2019-11-05 21:18:19 -0800

    update

--- neural_network.py
+++ neural_network.py
@@ -12,7 +12,7 @@ def predict(model, x):
 def build_model(X,y,nn_hdim, num_passes=20000,printloss=False):
     sampleSize = np.size(X, 1)
     print(sampleSize)
-    step = 0.0001
+    step = 0.01
     b1 = np.zeros((1,nn_hdim))
     b2 = np.zeros((1,nn_hdim))
     nn = None
@@ -25,8 +25,8 @@ def build_model(X,y,nn_hdim, num_passes=20000,printloss=False):
         h = np.tanh(a)
         z = h.dot(W2) + b2
         y1 = softmax(z)
-        if(math.isnan(y1[0][0])):
-            print(i)
+        #if(math.isnan(y1[0][0])):
+            #print(i)
 
         back2 = y1
         a1 = 1 - ((np.tanh(a))**2)
@@ -34,22 +34,22 @@ def build_model(X,y,nn_hdim, num_passes=20000,printloss=False):
             for j in range(nn_hdim):
                 back2[k][j] = back2[k][j] - y[j]
 
-        dW2 = (h.T).dot(back2)
-        db2 = np.sum(back2)
+        gW2 = (h.T).dot(back2)
+        gb2 = np.sum(back2, axis=0, keepdims=True)
 
         back1 = a1 * back2.dot(W2.T)
-        dW1 = np.dot(X.T, back1)
-        db1 = np.sum(back1, axis=0)
+        gW1 = np.dot(X.T, back1)
+        gb1 = np.sum(back1, axis=0)
         if(i % 5000 == 0):
-            print(W1, W2, dW1, dW2)
+            print(W1, W2, gW1, gW2)
 
-        dW2 += 0.01 * W2
-        dW1 += 0.01 * W1
+        #gW2 += 0.01 * W2
+        #gW1 += 0.01 * W1
 
-        W1 = W1 - (step * dW1)
-        b1 = b1 - (step * db1)
-        W2 = W2 - (step * dW2)
-        b2 = b2 - (step * db2)
+        W1 = W1 - (step * gW1)
+        b1 = b1 - (step * gb1)
+        W2 = W2 - (step * gW2)
+        b2 = b2 - (step * gb2)
 
 
         nn = {'W1': W1, 'b1': b1, 'W2':W2, 'b2': b2}
@@ -62,6 +62,7 @@ def build_model(X,y,nn_hdim, num_passes=20000,printloss=False):
 def softmax(z):
     i = np.exp(z)
     y = i / np.sum(i, axis=1, keepdims=True)
+    #print("Sum of i is this", np.sum(i, axis=1, keepdims=True))
     return y
 
 def plot_decision_boundary(pred_func, X, y):
